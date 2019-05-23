8336928025386cb2c39f6d852a327bc2df901c1b
jongsoo <soohappykjs7@naver.com>
2019-05-23 17:27:40 +0900

    5.23 대각선적, pilot기능개선
    시간에 따라 적이 점점 더 많이 출현하게 하고 대각선으로도 나오게 구현 pilot도 대각선으로 움직일 수 있게 해보았다.

--- app/src/main/java/com/example/soo/grabbing/AvoidGame.java
+++ app/src/main/java/com/example/soo/grabbing/AvoidGame.java
@@ -177,7 +177,7 @@ class ViewEx2 extends View
     private Paint enemy5=new Paint();
     private Paint enemy6=new Paint();
     private Paint enemy7=new Paint();
-    private int pilotSpeed=5;
+    private int pilotSpeed=3;
     private int pilotX=500;
     private int pilotY=1000;
 
@@ -186,6 +186,35 @@ class ViewEx2 extends View
 
     private int enemy2Speed=15;
     private int enemy2X,enemy2Y;
+
+    private int enemy3Speed=22;
+    private int enemy3X,enemy3Y;
+
+    private int enemy4Speed=17;
+    private int enemy4X,enemy4Y;
+
+    private int enemy5Speed=25;
+    private int enemy5X,enemy5Y;
+
+    private int enemy6Speed=13;
+    private int enemy6X,enemy6Y;
+
+    private int enemy7Speed=14;
+    private int enemy7X,enemy7Y;
+
+    private int duration=0;
+    private Canvas common_canvas;
+
+    private boolean enemyboolean1=true;
+    private boolean enemyboolean2=true;
+    private boolean enemyboolean3=true;
+    private boolean enemyboolean4=true;
+    private boolean enemyboolean5=true;
+    private boolean enemyboolean6=true;
+    private boolean enemyboolean7=true;
+
+
+
     //////////
     private int fishX=10;
     private int fishY;
@@ -223,7 +252,7 @@ class ViewEx2 extends View
         scorePaint.setTypeface(Typeface.DEFAULT_BOLD);
         scorePaint.setAntiAlias(true);
 
-        text.setColor(Color.BLACK);
+        text.setColor(Color.WHITE);
         text.setTextSize(30);
         text.setAntiAlias(true);
 /////////
@@ -260,13 +289,15 @@ class ViewEx2 extends View
         canvas.drawBitmap(background_space,0,0,null);
         canvasWidth=canvas.getWidth();
         canvasHeight=canvas.getHeight();
-
-        Log.e("hihi",String.valueOf(canvasWidth)+"aaa"+String.valueOf(canvasHeight));
+        common_canvas=canvas;
+        phase();
+        Log.e("hihii",String.valueOf(canvasWidth)+"aaa"+String.valueOf(canvasHeight));
 //        canvas.drawText("Ax="+a[0]+" Ay="+a[1]+" Az="+a[2]+"\n"+"온도="+a[3]+"\n"+"Gx="+a[4]+" Gy="+a[5]+" Gz="+a[6] +"\n"+"Pitch="+a[7]+" Roll="+a[8]+" Yaw="+a[9], 20, 500, text);
         canvas.drawText("Pitch="+a[7]+" Roll="+a[8]+" Yaw="+a[9], (int)canvasWidth*2/3+6, 300, text);
         text.setTextSize(22);
         RPYsensor();
-        canvas.drawCircle(pilotX,pilotY,30,pilot);
+//        canvas.drawCircle(pilotX,pilotY,30,pilot);
+        canvas.drawBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.pilot),pilotX,pilotY,pilot);
 //        greenY=greenY +greenSpeed;
         enemyY=enemyY +enemySpeed;
 
@@ -303,6 +334,7 @@ class ViewEx2 extends View
                 getContext().startActivity(gameOverIntent);
 
             }
+            duration++;
 
         }
         if(enemy2Y >canvasHeight+30)
@@ -330,7 +362,174 @@ class ViewEx2 extends View
 
             }
             green=false;
+            duration++;
         }
+        if(enemy2Y >canvasHeight+30)
+        {
+            if(scorecheck!=score-10) {
+                lifeCounter--;
+
+            }
+            else  scorecheck += 10;
+
+            enemy2Y=0;
+            int random2=(int)Math.floor(Math.random() *(canvasWidth));
+            enemy2X=random2;
+            once=true;
+
+            if (lifeCounter==0)
+            {
+                Toast.makeText(getContext(),"Game over", Toast.LENGTH_SHORT).show();
+
+                Intent gameOverIntent =new Intent(getContext(),GameOverActivity.class);
+                gameOverIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                gameOverIntent.putExtra("score",score);
+
+                getContext().startActivity(gameOverIntent);
+
+            }
+            green=false;
+            duration++;
+        }
+
+        if(enemy3Y >canvasHeight+30)
+        {
+            if(scorecheck!=score-10) {
+                lifeCounter--;
+
+            }
+            else  scorecheck += 10;
+
+            enemy3Y=0;
+            int random3=(int)Math.floor(Math.random() *(canvasWidth));
+            enemy3X=random3;
+            once=true;
+
+            if (lifeCounter==0)
+            {
+                Toast.makeText(getContext(),"Game over", Toast.LENGTH_SHORT).show();
+
+                Intent gameOverIntent =new Intent(getContext(),GameOverActivity.class);
+                gameOverIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                gameOverIntent.putExtra("score",score);
+
+                getContext().startActivity(gameOverIntent);
+
+            }
+            green=false;
+            duration++;
+        }
+        if(enemy4Y >canvasHeight+30)
+        {
+            if(scorecheck!=score-10) {
+                lifeCounter--;
+
+            }
+            else  scorecheck += 10;
+
+            enemy4Y=0;
+            int random4=(int)Math.floor(Math.random() *(canvasWidth));
+            enemy4X=random4;
+            once=true;
+
+            if (lifeCounter==0)
+            {
+                Toast.makeText(getContext(),"Game over", Toast.LENGTH_SHORT).show();
+
+                Intent gameOverIntent =new Intent(getContext(),GameOverActivity.class);
+                gameOverIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                gameOverIntent.putExtra("score",score);
+
+                getContext().startActivity(gameOverIntent);
+
+            }
+            green=false;
+            duration++;
+        }
+        if(enemy5Y >canvasHeight+30)
+        {
+            if(scorecheck!=score-10) {
+                lifeCounter--;
+
+            }
+            else  scorecheck += 10;
+
+            enemy5Y=0;
+            int random5=(int)Math.floor(Math.random() *(canvasWidth));
+            enemy5X=random5;
+            once=true;
+
+            if (lifeCounter==0)
+            {
+                Toast.makeText(getContext(),"Game over", Toast.LENGTH_SHORT).show();
+
+                Intent gameOverIntent =new Intent(getContext(),GameOverActivity.class);
+                gameOverIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                gameOverIntent.putExtra("score",score);
+
+                getContext().startActivity(gameOverIntent);
+
+            }
+            green=false;
+            duration++;
+        }
+        if(enemy6Y >canvasHeight+30)
+        {
+            if(scorecheck!=score-10) {
+                lifeCounter--;
+
+            }
+            else  scorecheck += 10;
+
+            enemy6Y=0;
+            int random6=(int)Math.floor(Math.random() *(canvasWidth));
+            enemy6X=random6;
+            once=true;
+
+            if (lifeCounter==0)
+            {
+                Toast.makeText(getContext(),"Game over", Toast.LENGTH_SHORT).show();
+
+                Intent gameOverIntent =new Intent(getContext(),GameOverActivity.class);
+                gameOverIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                gameOverIntent.putExtra("score",score);
+
+                getContext().startActivity(gameOverIntent);
+
+            }
+            green=false;
+            duration++;
+        }
+        if(enemy7Y >canvasHeight+30)
+        {
+            if(scorecheck!=score-10) {
+                lifeCounter--;
+
+            }
+            else  scorecheck += 10;
+
+            enemy7Y=0;
+            int random7=(int)Math.floor(Math.random() *(canvasWidth));
+            enemy7X=random7;
+            once=true;
+
+            if (lifeCounter==0)
+            {
+                Toast.makeText(getContext(),"Game over", Toast.LENGTH_SHORT).show();
+
+                Intent gameOverIntent =new Intent(getContext(),GameOverActivity.class);
+                gameOverIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                gameOverIntent.putExtra("score",score);
+
+                getContext().startActivity(gameOverIntent);
+
+            }
+            green=false;
+            duration++;
+        }
+
+
+
 
         if(once==true) {
                 score = score + 10;
@@ -357,7 +556,13 @@ class ViewEx2 extends View
         canvas.drawText("Score : "+score, 20, 150, scorePaint);
     }
 
-
+    public boolean hitBallChecker(int x,int y)
+    {
+        if(pilotX <x && x<(pilotX+ BitmapFactory.decodeResource(getResources(),R.drawable.pilot).getWidth()) && pilotY<y && y< (pilotY+ BitmapFactory.decodeResource(getResources(),R.drawable.pilot).getHeight())){
+            return true;
+        }
+        return false;
+    }
     public boolean onTouchEvent(MotionEvent event) {
         if(event.getAction()==MotionEvent.ACTION_DOWN){
             touchx=(int)event.getX();
@@ -367,46 +572,106 @@ class ViewEx2 extends View
         }
         return true;
     }
+    public void phase(){
+        //시간이 지날수록 점점 난이도가 올라가게 구현 장애물 아래로 떨어지면 duration의 값 올라가는데 그 개수의 기준으로 난이도 업
+        if(duration>3){
+            if(enemyboolean3==true){
+                enemy3Y=0;
+                int random3=(int)Math.floor(Math.random() *(canvasWidth));
+                enemy3X=random3;
+                enemyboolean3=false;
+            }
+            enemy3Y=enemy3Y +enemy2Speed;
+            enemy3X=enemy3X +7;
+            common_canvas.drawCircle(enemy3X,enemy3Y,25,enemy3);
+
+            if(enemyboolean4==true){
+                enemy4Y=0;
+                int random4=(int)Math.floor(Math.random() *(canvasWidth));
+                enemy4X=random4;
+                enemyboolean4=false;
+            }
+            enemy4Y=enemy4Y +enemy4Speed;
+            enemy4X=enemy4X -3;
+            common_canvas.drawCircle(enemy4X,enemy4Y,25,enemy4);
+        }
+        if(duration>5) {
+            if(enemyboolean5==true){
+                enemy5Y=0;
+                int random5=(int)Math.floor(Math.random() *(canvasWidth));
+                enemy5X=random5;
+                enemyboolean5=false;
+            }
+            enemy5Y=enemy5Y +enemy4Speed;
+            enemy5X=enemy5X -10;
+            common_canvas.drawCircle(enemy5X,enemy5Y,25,enemy5);
+        }
+        if(duration>7){
+
+            if(enemyboolean6==true){
+                enemy6X=0;
+                int random6=(int)Math.floor(Math.random() *(canvasWidth));
+                enemy6Y=random6;
+                enemyboolean6=false;
+            }
+            enemy6Y=enemy6Y -7;
+            enemy6X=enemy6X +enemy6Speed;
+            common_canvas.drawCircle(enemy6X,enemy6Y,25,enemy6);
+
+        }
+        if (duration>11){
+            if(enemyboolean7==true){
+                enemy7X=canvasWidth;
+                int random7=(int)Math.floor(Math.random() *(canvasWidth));
+                enemy7Y=random7;
+                enemyboolean7=false;
+            }
+            enemy7Y=enemy7Y +15;
+            enemy7X=enemy7X -enemy7Speed;
+            common_canvas.drawCircle(enemy7X,enemy7Y,25,enemy7);
+
+        }
+    }
 
     public void RPYsensor(){
     Log.e("Checkexersize1","Ax="+a[0]+" Ay="+a[1]+" Az="+a[2]+"\n"+"온도="+a[3]+"\n"+"Gx="+a[4]+" Gy="+a[5]+" Gz="+a[6] +"\n"+"Pitch="+a[7]+" Roll="+a[8]+" Yaw="+a[9]);
     //이두 roll값 40이상 80이하 pitch +-45 이하
 
-        if(Float.valueOf(a[7])>0){
-            //pitch값 +되면 왼쪽
+        if(Float.valueOf(a[7])>0&&Float.valueOf(a[8])>-30&&Float.valueOf(a[8])<30){
+            //pitch값 +되면 왼쪽  roll +-30이내
             pilotX=pilotX-pilotSpeed;
         }
-        else if(Float.valueOf(a[7])<0){
+        else if(Float.valueOf(a[7])<0&&Float.valueOf(a[8])>-30&&Float.valueOf(a[8])<30){
             //pitch값 -되면 오른쪽
             pilotX=pilotX+pilotSpeed;
         }
-        else if(Float.valueOf(a[8])>0){
+        else if(Float.valueOf(a[8])>0&&Float.valueOf(a[7])>-30&&Float.valueOf(a[7])<30){
             //roll값 +되면 아래
             pilotY=pilotY+pilotSpeed;
         }
-        else if(Float.valueOf(a[8])<0){
+        else if(Float.valueOf(a[8])<0&&Float.valueOf(a[7])>-30&&Float.valueOf(a[7])<30){
             //roll값 -되면 위
             pilotY=pilotY-pilotSpeed;
         }
-        else if(Float.valueOf(a[7])>0&&Float.valueOf(a[8])<0){
+        else if(Float.valueOf(a[7])>=30&&Float.valueOf(a[8])<=-30){
             //pitch값 +되면 왼쪽//roll값 -되면 위 ->왼쪽위
-            pilotY=pilotY-pilotSpeed+3;
-            pilotX=pilotX-pilotSpeed+3;
+            pilotY=pilotY-pilotSpeed;
+            pilotX=pilotX-pilotSpeed;
         }
-        else if(Float.valueOf(a[7])<0&&Float.valueOf(a[8])<0){
+        else if(Float.valueOf(a[7])<=-30&&Float.valueOf(a[8])<=-30){
             //pitch값 -되면 오른쪽//roll값 -되면 위 ->오른쪽위
-            pilotY=pilotY-pilotSpeed+3;
-            pilotX=pilotX+pilotSpeed-3;
+            pilotY=pilotY-pilotSpeed;
+            pilotX=pilotX+pilotSpeed;
         }
-        else if(Float.valueOf(a[7])>0&&Float.valueOf(a[8])>0){
+        else if(Float.valueOf(a[7])>=30&&Float.valueOf(a[8])>=30){
             //pitch값 +되면 왼쪽//roll값 +되면 아래 ->왼쪽아래
-            pilotY=pilotY-pilotSpeed+3;
-            pilotX=pilotX-pilotSpeed+3;
+            pilotY=pilotY+pilotSpeed;
+            pilotX=pilotX-pilotSpeed;
         }
-        else if(Float.valueOf(a[7])<0&&Float.valueOf(a[8])>0){
+        else if(Float.valueOf(a[7])<-30&&Float.valueOf(a[8])>30){
             //pitch값 -되면 오른쪽//roll값 +되면 아래 ->오른쪽아래
-            pilotY=pilotY-pilotSpeed+3;
-            pilotX=pilotX-pilotSpeed+3;
+            pilotY=pilotY+pilotSpeed;
+            pilotX=pilotX+pilotSpeed;
         }
 
 
